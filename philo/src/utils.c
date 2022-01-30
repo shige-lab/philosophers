1a899d54503d5f918b9e3173b3991c049b7edba5
shige <shigebask@gmail.com>
2022-01-31 01:59:42 +0900

    add: utils.c

---
+++ philo/src/utils.c
@@ -0,0 +1,27 @@
+#include "philo.h"
+
+size_t	get_current_time(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+}
+
+size_t	get_left_index(size_t pthread_num, size_t rihgt_index)
+{
+	if (rihgt_index == 0)
+		return (pthread_num - 1);
+	return (rihgt_index - 1);
+}
+
+void	put_log(pthread_mutex_t *log, char *action, size_t pthread_index)
+{
+	while (true)
+	{
+		if (pthread_mutex_lock(log) == 0)
+			break ;
+	}
+	printf("%zu %zu %s\n", get_current_time(), pthread_index + 1, action);
+	pthread_mutex_unlock(log);
+}
