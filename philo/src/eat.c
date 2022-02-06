fcfefd2f214b73aa4ab3006643d528641d4a916f
shige <shigebask@gmail.com>
2022-02-06 22:52:59 +0900

    fix: test

--- philo/src/eat.c
+++ philo/src/eat.c
@@ -10,8 +10,8 @@ void	eating(t_philo *philo, size_t th_index)
 	if (philo->is_dead == true)
 		return ;
 	action_time(philo->eat_time);
-	pthread_mutex_unlock(&philo->fork[left_index]);
-	pthread_mutex_unlock(&philo->fork[th_index]);
+	pthread_mutex_unlock(philo->fork + left_index);
+	pthread_mutex_unlock(philo->fork + th_index);
 	if (philo->eat_limit)
 		philo->eat_times[th_index] += 1;
 }
